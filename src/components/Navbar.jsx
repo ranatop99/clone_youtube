9e73d2827a04810d4e688228a76b7a69eff62cf7
Rana1017 <toprana212@gmail.com>
2023-06-24 18:17:36 +0545

    Created necessary components & set up files and folders

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1> NavBar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
