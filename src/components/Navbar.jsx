9959a9967a5451c4672b37447b6ebb84b0a22d05
Rana1017 <toprana212@gmail.com>
2023-06-25 22:38:58 +0545

    Created new components as well as created Sidebar components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,26 @@
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
+import { logo } from "../utils/constant";
+import SearchBar from "../components/SearchBar";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1> NavBar</h1>
-    </div>
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        position: "sticky",
+        background: "#000",
+        top: 0,
+        justifyContent: "space-between",
+      }}
+    >
+      <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+        <img src={logo} alt="logo" height={45} />
+      </Link>
+      <SearchBar />
+    </Stack>
   );
 };
 
